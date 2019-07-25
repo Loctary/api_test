38920b8076e5aaaf3048a2b26f6a966fe75357fe
Vasyl Butov <loctary@mail.ru>
2019-07-25 15:09:10 +0300

    Updates

---
+++ precache-manifest.40f0768a41cabf98796934d0f8dd9247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2eb3414015b2fd2b55495fee3928e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe5a7da7bcbbfb94ded4",
+    "url": "/static/css/main.369c4c1a.chunk.css"
+  },
+  {
+    "revision": "fc04ef4e77ae71ed673a",
+    "url": "/static/js/2.5f09221e.chunk.js"
+  },
+  {
+    "revision": "fe5a7da7bcbbfb94ded4",
+    "url": "/static/js/main.b2a0e1ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
